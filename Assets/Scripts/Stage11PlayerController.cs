818b6897e56abbd359aee3a0e13fc6fefebb9d4d
EQ1 <bluetwintail@naver.com>
2016-08-15 00:34:24 +0900

    Try To Make 1-1 Map

--- Assets/Scripts/Stage11PlayerController.cs
+++ Assets/Scripts/Stage11PlayerController.cs
@@ -3,12 +3,9 @@ using System.Collections;
 
 public class Stage11PlayerController : MonoBehaviour
 {
-	Vector3 PlayerPos;
+	public GameObject GroundChecker;
 
-	void Start()
-	{
-		
-	}
+	Vector3 PlayerPos;
 
 	void Update()
 	{
@@ -16,10 +13,18 @@ public class Stage11PlayerController : MonoBehaviour
 
 		if (PosCheck(8, 2) && Input.GetKeyDown (KeyCode.RightArrow))
 			RightTeleportMove (6);
+		else if (PosCheck(10, 2) && Input.GetKeyDown (KeyCode.LeftArrow))
+			Stop();
+		else if (PosCheck(14, 2) && Input.GetKeyDown (KeyCode.RightArrow))
+			RightTeleportMove (10);
 		else if (PosCheck(16, 2) && Input.GetKeyDown (KeyCode.LeftArrow))
 			LeftTeleportMove (6);
 		else if (PosCheck(24, 2) && Input.GetKeyDown (KeyCode.RightArrow))
 			RightTeleportMove (2);
+		else if (PosCheck(26, 2) && Input.GetKeyDown (KeyCode.LeftArrow))
+			LeftTeleportMove (10);
+		else if (PosCheck(26, 2) && Input.GetKeyDown (KeyCode.RightArrow))
+			RightTeleportMove (4);
 		else if (PosCheck(28, 2) && Input.GetKeyDown (KeyCode.LeftArrow))
 			LeftTeleportMove (2);
 		else if (PosCheck(28, 2) && Input.GetKeyDown (KeyCode.UpArrow))
@@ -32,9 +37,17 @@ public class Stage11PlayerController : MonoBehaviour
 			DownTeleportMove (4);
 		else if (PosCheck(30, 2) && Input.GetKeyDown (KeyCode.RightArrow))
 			RightTeleportMove (8);
+		else if (PosCheck(32, 2) && Input.GetKeyDown (KeyCode.LeftArrow))
+			LeftTeleportMove (4);
+		else if (PosCheck(38, 2) && Input.GetKeyDown (KeyCode.RightArrow))
+			RightTeleportMove (4);
 		else if (PosCheck(40, 2) && Input.GetKeyDown (KeyCode.LeftArrow))
 			LeftTeleportMove (8);
 		else if (PosCheck(42, 2) && Input.GetKeyDown (KeyCode.RightArrow))
+			Stop ();
+		else if (PosCheck(44, 2) && Input.GetKeyDown (KeyCode.LeftArrow))
+			LeftTeleportMove (4);
+		else if (PosCheck(0, 2) && Input.GetKeyDown (KeyCode.LeftArrow))
 			Stop ();
 		else
 			NormalMove ();
@@ -49,6 +62,14 @@ public class Stage11PlayerController : MonoBehaviour
 		else if (Input.GetKeyDown (KeyCode.LeftArrow))
 		{
 			GetComponent<Transform> ().position = new Vector3 (PlayerPos.x - 2, PlayerPos.y, -1);
+		}
+		else if (Input.GetKeyDown (KeyCode.UpArrow))
+		{
+			GetComponent<Transform> ().position = new Vector3 (PlayerPos.x, PlayerPos.y + 2, -1);
+		}
+		else if (Input.GetKeyDown (KeyCode.DownArrow))
+		{
+			GetComponent<Transform> ().position = new Vector3 (PlayerPos.x, PlayerPos.y - 2, -1);
 		}
 	}
 
