82971f80fe5407580d70c800d5bf133bc9ab3c29
AirHorizons <guardian1248@snu.ac.kr>
2016-08-15 06:03:34 +0900

    Linked Levels by LevelSelect Scene

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class NextLevel : MonoBehaviour {
+
+    public string titleScene;
+    public string settingScene;
+    public string levelSelect;
+
+    public NextLevel()
+    {
+        titleScene = "Title";
+        settingScene = "Settings";
+        levelSelect = "LevelSelect";
+    }
+
+	public void gotoLevel(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void loadMainMenu()
+    {
+        SceneManager.LoadScene(titleScene);
+    }
+
+    public void loadLevelSelect()
+    {
+        SceneManager.LoadScene(levelSelect);
+    }
+
+    public void gotoLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+}
