bd1387abe49242feccd3aba5e8a63bb7f1d6d0b9
AirHorizons <guardian1248@snu.ac.kr>
2016-08-15 04:03:30 +0900

    Add SEManager

---
+++ Assets/Scripts/SEManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SEManager : MonoBehaviour {
+
+    public enum Sounds
+    {
+        Move1,
+        Move2,
+        Blink,
+        Click,
+        StageClear
+    }
+
+    public List<AudioClip> SEList;
+
+    public void Play(Sounds sounds)
+    {
+        AudioSource audiosource = GetComponent<AudioSource>();
+        audiosource.clip = SEList[(int)sounds];
+        audiosource.Play();
+    }
+}
