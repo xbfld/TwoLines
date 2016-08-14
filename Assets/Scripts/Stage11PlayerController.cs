734eca368cee8901be1051c88a5ec544f2e3f106
AirHorizons <guardian1248@snu.ac.kr>
2016-08-15 08:31:07 +0900

    Modify 1-1 Player Script

--- Assets/Scripts/Stage11PlayerController.cs
+++ Assets/Scripts/Stage11PlayerController.cs
@@ -5,6 +5,7 @@ public class Stage11PlayerController : MonoBehaviour
 {
 	Animator Anim;
 	bool IsMoving;
+    bool IsMovable { get; set; }
 	float NowTime;
 	float StartTime;
 	float StartXPosition;
@@ -18,54 +19,56 @@ public class Stage11PlayerController : MonoBehaviour
         SeManager = GetComponent<SEManager>();
         if (SeManager != null) Debug.Log("Component Get");
         else Debug.Log("Failed to Get SEManager");
+        IsMovable = true;
 	}
 
 	void Update()
 	{
 		PlayerPos = GetComponent<Transform> ().position;
 
-		if (PosCheck (8, 2) && Input.GetKeyDown (KeyCode.RightArrow))
-			RightTeleportMove (6);
-		else if (PosCheck (10, 2) && Input.GetKeyDown (KeyCode.LeftArrow))
-			Stop ();
-		else if (PosCheck (14, 2) && Input.GetKeyDown (KeyCode.RightArrow))
-			RightTeleportMove (10);
-		else if (PosCheck (16, 2) && Input.GetKeyDown (KeyCode.LeftArrow))
-			LeftTeleportMove (6);
-		else if (PosCheck (24, 2) && Input.GetKeyDown (KeyCode.RightArrow))
-			RightTeleportMove (2);
-		else if (PosCheck (26, 2) && Input.GetKeyDown (KeyCode.LeftArrow))
-			LeftTeleportMove (10);
-		else if (PosCheck (26, 2) && Input.GetKeyDown (KeyCode.RightArrow))
-			RightTeleportMove (4);
-		else if (PosCheck (28, 2) && Input.GetKeyDown (KeyCode.LeftArrow))
-			LeftTeleportMove (2);
-		else if (PosCheck (28, 2) && Input.GetKeyDown (KeyCode.UpArrow))
-			UpTeleportMove (4);
-		else if (PosCheck (28, 10) && Input.GetKeyDown (KeyCode.DownArrow))
-			DownTeleportMove (4);
-		else if (PosCheck (30, 2) && Input.GetKeyDown (KeyCode.UpArrow))
-			UpTeleportMove (4);
-		else if (PosCheck (30, 10) && Input.GetKeyDown (KeyCode.RightArrow))
-			GetComponent<Transform> ().position = new Vector3 (32, 2, -1);
-		else if (PosCheck(30, 10) && Input.GetKeyDown (KeyCode.DownArrow))
-			DownTeleportMove (4);
-		else if (PosCheck(30, 2) && Input.GetKeyDown (KeyCode.RightArrow))
-			RightTeleportMove (8);
-		else if (PosCheck(32, 2) && Input.GetKeyDown (KeyCode.LeftArrow))
-			LeftTeleportMove (4);
-		else if (PosCheck(38, 2) && Input.GetKeyDown (KeyCode.RightArrow))
-			RightTeleportMove (4);
-		else if (PosCheck(40, 2) && Input.GetKeyDown (KeyCode.LeftArrow))
-			LeftTeleportMove (8);
-		else if (PosCheck(42, 2) && Input.GetKeyDown (KeyCode.RightArrow))
-			Stop ();
-		else if (PosCheck(44, 2) && Input.GetKeyDown (KeyCode.LeftArrow))
-			LeftTeleportMove (4);
-		else if (PosCheck(0, 2) && Input.GetKeyDown (KeyCode.LeftArrow))
-			Stop ();
-		else
-			NormalMove ();
+        if (IsMovable)
+        {
+            if (PosCheck(8, 2) && Input.GetKeyDown(KeyCode.RightArrow))
+                RightTeleportMove(6);
+            else if (PosCheck(10, 2) && Input.GetKeyDown(KeyCode.LeftArrow))
+                Stop();
+            else if (PosCheck(14, 2) && Input.GetKeyDown(KeyCode.RightArrow))
+                RightTeleportMove(10);
+            else if (PosCheck(16, 2) && Input.GetKeyDown(KeyCode.LeftArrow))
+                LeftTeleportMove(6);
+            else if (PosCheck(24, 2) && Input.GetKeyDown(KeyCode.RightArrow))
+                RightTeleportMove(2);
+            else if (PosCheck(26, 2) && Input.GetKeyDown(KeyCode.LeftArrow))
+                LeftTeleportMove(10);
+            else if (PosCheck(26, 2) && Input.GetKeyDown(KeyCode.RightArrow))
+                RightTeleportMove(4);
+            else if (PosCheck(28, 2) && Input.GetKeyDown(KeyCode.LeftArrow))
+                LeftTeleportMove(2);
+            else if (PosCheck(28, 2) && Input.GetKeyDown(KeyCode.UpArrow))
+                UpTeleportMove(4);
+            else if (PosCheck(28, 10) && Input.GetKeyDown(KeyCode.DownArrow))
+                DownTeleportMove(4);
+            else if (PosCheck(30, 2) && Input.GetKeyDown(KeyCode.UpArrow))
+                UpTeleportMove(4);
+            else if (PosCheck(30, 10) && Input.GetKeyDown(KeyCode.DownArrow))
+                DownTeleportMove(4);
+            else if (PosCheck(30, 2) && Input.GetKeyDown(KeyCode.RightArrow))
+                RightTeleportMove(8);
+            else if (PosCheck(32, 2) && Input.GetKeyDown(KeyCode.LeftArrow))
+                LeftTeleportMove(4);
+            else if (PosCheck(38, 2) && Input.GetKeyDown(KeyCode.RightArrow))
+                RightTeleportMove(4);
+            else if (PosCheck(40, 2) && Input.GetKeyDown(KeyCode.LeftArrow))
+                LeftTeleportMove(8);
+            else if (PosCheck(42, 2) && Input.GetKeyDown(KeyCode.RightArrow))
+                Stop();
+            else if (PosCheck(44, 2) && Input.GetKeyDown(KeyCode.LeftArrow))
+                LeftTeleportMove(4);
+            else if (PosCheck(0, 2) && Input.GetKeyDown(KeyCode.LeftArrow))
+                Stop();
+            else
+                NormalMove();
+        }
 	}
 
 	void NormalMove()
