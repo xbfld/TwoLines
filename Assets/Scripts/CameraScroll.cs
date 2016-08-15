951067c969c9014cc221f2ffe65a4754a0eeacda
EQ1 <bluetwintail@naver.com>
2016-08-15 09:19:23 +0900

    Modify Camera Scroller

--- Assets/Scripts/CameraScroll.cs
+++ Assets/Scripts/CameraScroll.cs
@@ -1,23 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraScroll : MonoBehaviour {
+public class CameraScroll : MonoBehaviour
+{
+	public int CameraStartPosition;
+	public int CameraEndPosition;
+
     GameObject player = null;
     Vector3 playerPos;
 
-    // 카메라의 position은 기본적으로 y-value를 8로 유지(돌바닥이 0에 깔려있고 높이가 2).
-    // 왼쪽으로는 17까지, 오른쪽으로는 캐릭터가 움직이는 곳까지(포탈이 맵의 중간에 있을 수 있음)
-
-	// Use this for initialization
-	void Start () {
-        player = GameObject.Find("Player"); // find player using its name
-        playerPos = player.transform.position;
-        this.transform.position = new Vector3((17 > playerPos.x ? 17 : playerPos.x), 8, -10);
+	void Start ()
+	{
+        player = GameObject.Find("Player");
+		this.transform.position = new Vector3(CameraStartPosition, 8, -10);
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+	void Update () 
+	{
         playerPos = player.transform.position;
-        this.transform.position = new Vector3((17 > playerPos.x ? 17 : playerPos.x), 8, -10);
+		if(CameraStartPosition > playerPos.x)
+			this.transform.position = new Vector3(CameraStartPosition, 8, -10);
+		else if(playerPos.x < CameraEndPosition)
+			this.transform.position = new Vector3(playerPos.x, 8, -10);
+		else
+			this.transform.position = new Vector3(CameraEndPosition, 8, -10);
     }
 }
