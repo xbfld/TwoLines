fa09303eebfe0377a27dd6ccbaf4f5c861c7f5de
AirHorizons <guardian1248@snu.ac.kr>
2016-08-15 09:06:04 +0900

    Add GameStateManager for Stage 1-1

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStateManager : MonoBehaviour {
+
+    Vector3 PlayerPos;
+    Vector3 PortalPos;
+	
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        PlayerPos = FindObjectOfType<Stage11PlayerController>().PlayerPos;
+        PortalPos = GameObject.FindGameObjectWithTag("Portal").transform.position;
+
+        if (PlayerPos == PortalPos) Debug.Log("Stage Clear!");
+    }
+    
+}
