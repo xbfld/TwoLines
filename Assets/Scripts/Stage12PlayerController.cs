d3c1e0a7bda5bfad3da4d3a427f61f32d3cc2ded
EQ1 <bluetwintail@naver.com>
2016-08-15 11:29:40 +0900

    Modify Map 1-2

--- Assets/Scripts/Stage12PlayerController.cs
+++ Assets/Scripts/Stage12PlayerController.cs
@@ -22,6 +22,8 @@ public class Stage12PlayerController : MonoBehaviour
 
 		if (PosCheck (4, 2) && Input.GetKeyDown (KeyCode.RightArrow))
 			RightTeleportMove (4);
+		else if (PosCheck (6, 6) && Input.GetKeyDown (KeyCode.RightArrow))
+			Stop ();
 		else if (PosCheck (8, 2) && Input.GetKeyDown (KeyCode.RightArrow))
 			RightTeleportMove (2);
 		else if (PosCheck (10, 2) && Input.GetKeyDown (KeyCode.RightArrow))
@@ -55,6 +57,10 @@ public class Stage12PlayerController : MonoBehaviour
 		
 		else if (PosCheck (6, 2) && Input.GetKeyDown (KeyCode.LeftArrow))
 			Stop ();
+		else if (PosCheck (6, 6) && Input.GetKeyDown (KeyCode.LeftArrow))
+			GetComponent<Transform> ().position = new Vector3 (4, 2, -1);
+		else if (PosCheck (8, 12) && Input.GetKeyDown (KeyCode.LeftArrow))
+			GetComponent<Transform> ().position = new Vector3 (6, 6, -1);
 		else if (PosCheck (10, 2) && Input.GetKeyDown (KeyCode.LeftArrow))
 			LeftTeleportMove (4);
 		else if (PosCheck (12, 2) && Input.GetKeyDown (KeyCode.LeftArrow))
@@ -93,6 +99,8 @@ public class Stage12PlayerController : MonoBehaviour
 		else if (PosCheck (30, 2) && Input.GetKeyDown (KeyCode.UpArrow))
 			UpTeleportMove (6);
 
+		else if (PosCheck (8, 12) && Input.GetKeyDown (KeyCode.DownArrow))
+			DownTeleportMove (6);
 		else if (PosCheck (16, 12) && Input.GetKeyDown (KeyCode.DownArrow))
 			DownTeleportMove (6);
 		else if (PosCheck (18, 12) && Input.GetKeyDown (KeyCode.DownArrow))
